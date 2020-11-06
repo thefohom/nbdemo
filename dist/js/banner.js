227967c7073c110dc7c1acd19c3e10847426f102
thefohom <234623590@qq.com>
2020-11-06 17:28:03 +0800

    update~~~1

--- dist/js/banner.js
+++ dist/js/banner.js
@@ -6,42 +6,42 @@
     * 4、确定默认移动方向
     * */
     //获取操作对象
-    var imgs=document.getElementById('images')
-    var box=document.getElementById('box')
-    var btn=document.getElementById('btn')
+    var bnimgs=document.getElementById('bnimgs')
+    var bnbox=document.getElementById('banner')
+    var bnbtn=document.getElementById('bnbtn')
     var prev=document.getElementById('prev')
     var next=document.getElementById('next')
-    var spans=document.getElementsByTagName('span')
+    var spans=document.querySelectorAll('#bnbtn span')
     var index=1 //按钮的初始下标
     var dsq;//定时器
 
     var aa=false;// 当aa=true，代表还在执行定时器，那么就不能在执行其他定时器
     //移动图片
     function animation1(offset){
-        //当点击当前显示图片对应按钮时，直接跳出函数
+        //当点击当前显示图片对应按钮时，直接跳出函数 当移动距离为0的时候代表不移动，因此就不在执行下面的代码
         if(offset==0){
             return
         }
         //设置每一步的步长
         var speed=offset/40
         //获取图片移动终点
-        var left=parseInt(imgs.style.left)+offset
+        var left=parseInt(bnimgs.style.left)+offset
         aa=true
         //动起来
         var go= function () {
-            //从左往右移动                                    从右往左移动
-            if((speed>0 && parseInt(imgs.style.left)<left)||(speed<0 && parseInt(imgs.style.left)>left)){
-                //把计算结果赋值给imgs对象
-                imgs.style.left=parseInt(imgs.style.left)+speed+'px'
+            //从左往右移动 如果步长是正数且                       从右往左移动
+            if((speed>0 && parseInt(bnimgs.style.left)<left)||(speed<0 && parseInt(bnimgs.style.left)>left)){
+                //把计算结果赋值给bnimgs对象
+                bnimgs.style.left=parseInt(bnimgs.style.left)+speed+'px'
                 setTimeout(go,10)
             }else{
                 //next时，最后一张图片的判断
                 if(left<-1920*8){
-                    imgs.style.left='-1920px'
+                    bnimgs.style.left='-1920px'
                 }
                 //prev时，第一张图片的判断
                 if(left>-1920){
-                    imgs.style.left=-1920*8+"px"
+                    bnimgs.style.left=-1920*8+"px"
                 }
                 aa=false
             }
@@ -99,17 +99,17 @@
             //获取当前按钮对象的index属性值
             var myIndex=this.getAttribute('index')
             //显示按钮对应的图片
-//            imgs.style.left=-600*parseInt(myIndex)+'px'
+//            bnimgs.style.left=-600*parseInt(myIndex)+'px'
             animation1(-1920*(parseInt(myIndex)-index))
             index=myIndex
             btnShow()
         }
     }
     //鼠标移入删除定时器
-    box.onmouseover= function () {
+    bnbox.onmouseover= function () {
         clearInterval(dsq)
     }
-    box.onmouseout=play
+    bnbox.onmouseout=play
     play()
     /*
     * 完成剩下的功能
